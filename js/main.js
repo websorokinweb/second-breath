35fd886f6e16794386a37bbe8d7bda60579484ba
websorokinweb <73754839+websorokinweb@users.noreply.github.com>
2021-02-27 15:39:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(function () {
+    $("a").click(function(e){
+        e.preventDefault();
+    })
+
+    $('.header__slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        easing: 'linear',
+        cssEase: 'linear',
+        waitForAnimate: false,
+        speed: 25000,
+        centerMode: true,
+        edgeFriction: 0,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        autoSlidesToShow: true,
+        variableWidth: true,
+        swipe: false,
+        useTransform: false,
+        initialSlide: 1,
+    });
+
+    $('.header__slider-column--first').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 70000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--second').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 60000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--third').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 75000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--last').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 65000,
+        autoDirection: 'prev',
+    })
+
+
+
+});
