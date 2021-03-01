0f04aa40b6c16522cf45aa3cc994496e2669e8d1
websorokinweb <73754839+websorokinweb@users.noreply.github.com>
2021-03-01 16:42:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,105 @@
+$(function () {
+    // $("a").click(function(e){
+    //     e.preventDefault();
+    // })
+    
+
+    setTimeout(function() {
+        $('.logo__notify').removeClass('closed')
+    }, 2000);
+
+    setTimeout(function() {
+        $('.logo__notify').addClass('closed')
+    }, 8500);
+
+    // $(window).scroll(function () {
+    //     if (window.pageYOffset > 73) {
+    //         $('.header__bottom-panel').addClass('header__subscribe--relative')
+    //     } else {
+    //         $('.header__bottom-panel').removeClass('header__subscribe--relative')
+    //     }
+    // });
+
+    $('.header__slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        arrows: false,
+        // easing: 'linear',
+        // cssEase: 'linear',
+        waitForAnimate: false,
+        speed: 700000,
+        centerMode: true,
+        edgeFriction: 0,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        autoSlidesToShow: true,
+        variableWidth: true,
+        swipe: false,
+        useTransform: false,
+        initialSlide: 1,
+
+        init: true,
+        waitForAnimate: 1000,
+    });
+
+    $('.header__slider-column--first').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 70000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--second').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 60000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--third').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 75000,
+        autoDirection: 'prev',
+    })
+
+    $('.header__slider-column--last').bxSlider({
+        mode: 'vertical',
+        easing: 'linear',
+        ticker: true,
+        speed: 65000,
+        autoDirection: 'prev',
+    })
+
+    $('.logo__notify-cross').on('click', function(){
+        $('.logo__notify').addClass('closed')
+    })
+
+    $('.header__subscribe-cross').on('click', function(){
+        $('.header__subscribe').addClass('closed')
+    })
+
+    $('.tab').on('click', function(e) {
+        e.preventDefault();
+
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
+
+    $('.team__inner-more').on('click', function(){
+        $('.team__item-wrapper').removeClass('closed')
+        $('.team__item').removeClass('closed')
+        $(this).addClass('closed')
+        $('.team__item-social').removeClass('closed')
+        $('.team__items').addClass('all-active')
+    })
+
+});
